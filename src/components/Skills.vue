0af0f45091815fa37044b80fdc04ed6ee7eb646a
lukas <loe@fairytalesgroup.com>
2024-11-09 20:37:55 +0100

    Edited transition

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -10,32 +10,32 @@
         sm="6"
         md="3"
       >
-        <v-item v-slot="{ isSelected, selectedClass, toggle }">
+        <v-item v-slot="{ isSelected, selectedClass, isHovering, toggle }">
           <div class="card-container">
               <v-card class="text-behind">
                 <template v-slot:prepend>
                   <a 
                   :href="skill.link" 
-                  class="text-white"
+                  class="text-decoration-none d-flex align-center text-white"
                   >
-                <v-avatar :image="skill.link" rounded="0" class="ma-2"></v-avatar>
+                <v-avatar :image="skill.image" rounded="0" class="ma-2"></v-avatar>
               <v-card-title>{{skill.name}}</v-card-title>
               </a>
             </template>
-                <v-card-text>More about the wonders of the newly founded technology {{ skill.name }}</v-card-text>
+                <v-card-text>More text about {{ skill.name }}</v-card-text>
               </v-card>
           <v-card
-            :class="['d-flex align-center', selectedClass, {'selected-card': isSelected}]"
+            :class="['skill-card', selectedClass, {'selected-card': isSelected}]"
             height="200"
             color="black"
             @click="toggle"
+            
           >
-            <div class="text-h3 flex-grow-1 text-center">
               <v-img 
-                :src="skill.link"
+                :src="skill.image"
                 height="150"
               />
-            </div>
+              <v-icon class="door-handle">mdi-check</v-icon>
           </v-card>
         </div>
         </v-item>
@@ -50,10 +50,14 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 const skills = ref([
-  {id:1, name:"Supabase", link:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/supabase/supabase-original.svg", type:"technical"},
-  {id:2, name:"Vue", link:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg", type:"technical"},
-  {id:3, name:"TypeScript", link:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg", type:"technical"},
-  {id:4, name:"PostgreSQL", link:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg", type:"technical"}]);
+  {id:1, name:"Supabase", type:"technical", image:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/supabase/supabase-original.svg",link:"https://supabase.com"},
+  {id:2, name:"Vue", type:"technical", image:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg", link: "https://vuejs.org"},
+  {id:3, name:"TypeScript", type:"technical", image:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg", link: "https://www.typescriptlang.org"},
+  {id:4, name:"PostgreSQL", type:"technical", image:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg", link: "https://www.postgresql.org"},
+  {id:5, name:"Python", type:"technical", image:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg", link: "https://www.python.org"},
+  {id:6, name:"CSS", type:"technical", image:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg", link: "https://developer.mozilla.org/en-US/docs/Web/CSS"},
+  {id:7, name:"HTML", type:"technical", image:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg", link: "https://developer.mozilla.org/en-US/docs/Web/HTML"},
+  {id:8, name:"GitHub", type:"technical", image:"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg", link: "https://github.com"}]);
 </script>
 
 <style scoped>
@@ -72,22 +76,41 @@ const skills = ref([
   opacity: 1;
 }
 
-.v-card {
+.skill-card {
   transform-origin: left;
   transition: all 1.5s ease-in-out;
+  display: flex;
+  align-items: center;
 }
 
 .selected-card {
   transform: 
-  perspective(800px)
-  rotateY(-100deg);
+  perspective(1000px)
+  rotateY(-120deg);
+  border: 1px solid white;
+
+
 }
 
-.v-card:hover {
-  transform-style: preserve-3d;
+.skill-card:hover {
+  border: 1px solid white;
+
 }
 
-.selected-card ~ .text-behind {
+.door-handle {
+  opacity:0;
+  position:absolute;
+  right:8%;
+  transform: rotate(-135deg);
+  transition: opacity 1.5s ease-in-out;
+}
+.skill-card:hover .door-handle {
   opacity: 1;
+}
+
+.selected-card .door-handle {
+  opacity: 1;
+  transform: rotate(-155deg);
+  /* transition: transform 0.5s ease-out; */
 }
 </style>
